acc719427a6e96d28708ed8729fcc0dd11b2a062
Tom Coakes <tomcoakes@gmail.com>
2015-03-10 10:26:55 +0000

    Game can add two players, throws an error on three, and declares a draw when weapons are the same

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,12 +1,45 @@
 describe("Game Logic", function() {
 
+  beforeEach(function() {
+    game = new Game();
+    playerOne = new Player("Tom");
+    playerTwo = new Player("Petra");
+  });
+
   it("can add a player", function() {
-    var game = new Game();
-    var player = jasmine.createSpy('player');
-    game.addPlayer(player);
+    game.addPlayer(playerOne);
     expect(game.playerCount()).toEqual(1);
   });
 
+  it("can add two players", function() {
+    game.addPlayer(playerOne);
+    game.addPlayer(playerTwo);
+    expect(game.playerCount()).toEqual(2);
+  });
 
+  it("throws an error when a third player tries to enter", function() {
+    playerThree = jasmine.createSpy('Player');
+    game.addPlayer(playerOne);
+    game.addPlayer(playerTwo);
+    expect(function() {
+      game.addPlayer(playerThree);
+    }).toThrow(new Error("Only two people can play at a time"));
+  });
+
+  it("declares a draw when playerOne and playerTwo both choose 'paper'", function() {
+    game.addPlayer(playerOne);
+    game.addPlayer(playerTwo);
+    spyOn(playerOne, "chooseWeapon").and.returnValue("paper");
+    spyOn(playerTwo, "chooseWeapon").and.returnValue("paper");
+    expect(game.winner()).toBe("Draw");
+  });
+
+  it("declares that playerOne wins when they choose rock and playerTwo chooses scissors", function() {
+    game.addPlayer(playerOne);
+    game.addPlayer(playerTwo);
+    spyOn(playerOne, "chooseWeapon").and.returnValue("rock");
+    spyOn(playerTwo, "chooseWeapon").and.returnValue("scissors");
+    expect(game.winner()).toBe("Tom");
+  });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -3,9 +3,17 @@ var Game = function() {
 };
 
 Game.prototype.addPlayer = function(player) {
-  this.players.push(player);
+  if(this.playerCount() >= 2) {
+    throw new Error("Only two people can play at a time");
+  } else {
+    this.players.push(player);
+  }
 };
 
 Game.prototype.playerCount = function() {
   return this.players.length;
+};
+
+Game.prototype.winner = function() {
+  return "Draw";
 };
